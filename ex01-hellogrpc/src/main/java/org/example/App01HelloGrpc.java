25ef2e6c822a059a9804f36d1a6558cd30440dcf
Partha Biswas <biswas.partha1@gmail.com>
2025-03-09 16:09:36 +0530

    project setup

---
+++ ex01-hellogrpc/src/main/java/org/example/App01HelloGrpc.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App01HelloGrpc
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
