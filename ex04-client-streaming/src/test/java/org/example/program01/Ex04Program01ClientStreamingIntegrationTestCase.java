b1dd785e54c9523896765eb17545783538e478ed
Partha Biswas <biswas.partha1@gmail.com>
2025-03-19 23:51:58 +0530

    client-streaming: example01-program01 : integration-test-case

--- ex04-client-streaming/src/test/java/org/example/program01/Ex04Program01ClientStreamingIntegrationTestCase.java
+++ ex04-client-streaming/src/test/java/org/example/program01/Ex04Program01ClientStreamingIntegrationTestCase.java
@@ -1,5 +1,6 @@
 package org.example.program01;
 
+import com.google.common.util.concurrent.Uninterruptibles;
 import com.partha.program01.AccountBalance;
 import com.partha.program01.DepositRequest;
 import com.partha.program01.Money;
@@ -7,6 +8,7 @@ import org.example.common.ResponseObserver;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class Ex04Program01ClientStreamingIntegrationTestCase extends AbstractTest{
@@ -33,5 +35,18 @@ public class Ex04Program01ClientStreamingIntegrationTestCase extends AbstractTes
         Assertions.assertEquals( 1, responseObserver.getItems().size());
         Assertions.assertEquals(200, responseObserver.getItems().getFirst().getBalance());
         Assertions.assertNull(responseObserver.getThrowable());
+    }
+
+    //the below is not a test-case but to simulate what happens when the client cancels the streaming-request
+    @Test
+    void cancelledDeposit(){
+        var responseObserver = ResponseObserver.<AccountBalance>create();
+        var requestObserver = this.stub.deposit(responseObserver); //this works as the invoke call that we did via postman with empty message body
+
+        //initial message - account number
+        requestObserver.onNext(DepositRequest.newBuilder().setAccountNumber(5).build());
+
+        Uninterruptibles.sleepUninterruptibly(1, TimeUnit.SECONDS);
+        requestObserver.onError(new RuntimeException());
     }
 }
