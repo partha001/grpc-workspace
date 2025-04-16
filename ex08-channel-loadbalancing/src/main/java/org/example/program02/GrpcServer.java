a80b82e7bdf43c9a44c977b7d5cd06bc558385c5
Partha Biswas <biswas.partha1@gmail.com>
2025-04-16 17:42:32 +0530

    keep-alive: ex08-program02

--- ex08-channel-loadbalancing/src/main/java/org/example/program02/GrpcServer.java
+++ ex08-channel-loadbalancing/src/main/java/org/example/program02/GrpcServer.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
 public class GrpcServer {
 
@@ -13,45 +14,52 @@ public class GrpcServer {
 
     private final Server server;
 
-    private GrpcServer(Server server){
+    private GrpcServer(Server server) {
         this.server = server;
     }
 
-    public static GrpcServer create(BindableService... services){
-        return create(6565,services);
+    public static GrpcServer create(BindableService... services) {
+        return create(6565, services);
     }
 
-    public static GrpcServer create(int port, BindableService... services){
-        var builder = ServerBuilder.forPort(port);
+    public static GrpcServer create(int port, BindableService... services) {
+        //var builder = ServerBuilder.forPort(port);
+
+        var builder = ServerBuilder.forPort(port)
+                .keepAliveTime(10, TimeUnit.SECONDS)
+                .keepAliveTimeout(1, TimeUnit.SECONDS)
+                .maxConnectionIdle(25, TimeUnit.SECONDS);
+
+
         Arrays.asList(services).forEach(builder::addService);
         return new GrpcServer(builder.build());
     }
 
-    public GrpcServer start(){
+    public GrpcServer start() {
         //this is basically to print all the services that are registered to the server
         var services = server.getServices().stream().map(ServerServiceDefinition::getServiceDescriptor)
                 .map(ServiceDescriptor::getName)
                 .toList();
-        try{
+        try {
             server.start();
             log.info("server started. listening on port {}. services:{}", server.getPort(), services);
             return this;
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
 
-    public void await(){
-        try{
+    public void await() {
+        try {
             server.awaitTermination();
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
 
-    public void stop(){
+    public void stop() {
         server.shutdownNow();
         log.info("server stopped");
     }
--- ex08-channel-loadbalancing/src/test/java/org/example/program02/KeepAliveTest.java
+++ ex08-channel-loadbalancing/src/test/java/org/example/program02/KeepAliveTest.java
@@ -1,13 +1,16 @@
-package org.example.program01;
+package org.example.program02;
 
-import com.partha.program01.BalanceCheckRequest;
-import com.partha.program01.BankServiceGrpc;
+import com.google.common.util.concurrent.Uninterruptibles;
+import com.partha.program02.BalanceCheckRequest;
+import com.partha.program02.BankServiceGrpc;
 import org.example.common.AbstractChannelTest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * this class is to show eager channel creation behavior . however this is only for the purpose of demo.
@@ -33,8 +36,11 @@ public class KeepAliveTest extends AbstractChannelTest {
         var request = BalanceCheckRequest.newBuilder()
                 .setAccountNumber(1)
                 .build();
-//        var response = this.bankBlockingStub.getAccountBalance(request);
-//        log.info("{}", response);
+        var response = this.bankBlockingStub.getAccountBalance(request);
+        log.info("{}", response);
+
+        //just blocking the thread for 30 seconds
+        Uninterruptibles.sleepUninterruptibly(30, TimeUnit.SECONDS);
     }
 
     @AfterAll
