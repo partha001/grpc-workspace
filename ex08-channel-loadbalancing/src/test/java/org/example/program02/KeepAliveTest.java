fd382d42f6a18efa851ca59afc3f865ceba310df
Partha Biswas <biswas.partha1@gmail.com>
2025-04-17 00:06:59 +0530

    ex08-channel-loadbalancing : adding comment to the test client class

--- ex08-channel-loadbalancing/src/test/java/org/example/program02/KeepAliveTest.java
+++ ex08-channel-loadbalancing/src/test/java/org/example/program02/KeepAliveTest.java
@@ -13,12 +13,9 @@ import org.slf4j.LoggerFactory;
 import java.util.concurrent.TimeUnit;
 
 /**
- * this class is to show eager channel creation behavior . however this is only for the purpose of demo.
- * this is the standard-practice in the grpc world.
- * <p>
- * note: lazy creation of channel is the default behavior of rpc. i.e. the channel gets created on the first rpc-call made by the client.
- * however if there is a explicit requirement for eager initialization of channel then that can be done as shown below
- */
+ this test client is to see how ping and go away works between the client and server when we
+ we have set keepAliveTime and keepAliveTimeout on the server.
+  */
 public class KeepAliveTest extends AbstractChannelTest {
 
     private static final Logger log = LoggerFactory.getLogger(KeepAliveTest.class);
