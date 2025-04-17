8f80ab667886a4c0ea63017938bbd9cd49fbbeab
Partha Biswas <biswas.partha1@gmail.com>
2025-04-17 17:00:49 +0530

    ex08-channel-loadbalancing : program03: loadbalancing - user written files

---
+++ ex08-channel-loadbalancing/src/main/java/org/example/program03/LoadbalancingDemo.java
@@ -0,0 +1,20 @@
+package org.example.program03;
+
+public class LoadbalancingDemo {
+
+    private static class BankInstance1 {
+        public static void main(String[] args) {
+            GrpcServer.create(6565, new BankService())
+                    .start()
+                    .await();
+        }
+    }
+
+    private static class BankInstance2 {
+        public static void main(String[] args) {
+            GrpcServer.create(7575, new BankService())
+                    .start()
+                    .await();
+        }
+    }
+}
