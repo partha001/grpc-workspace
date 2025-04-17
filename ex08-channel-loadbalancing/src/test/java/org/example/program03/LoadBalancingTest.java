f010d93fa48291e9ed8800935f8966e34f6a281c
Partha Biswas <biswas.partha1@gmail.com>
2025-04-17 17:00:34 +0530

    ex08-channel-loadbalancing : program03: loadbalancing - generated files

---
+++ ex08-channel-loadbalancing/src/test/java/org/example/program03/LoadBalancingTest.java
@@ -0,0 +1,46 @@
+package org.example.program03;
+
+import com.partha.program03.BalanceCheckRequest;
+import com.partha.program03.BankServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class LoadBalancingTest {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadBalancingTest.class);
+    private BankServiceGrpc.BankServiceBlockingStub bankServiceBlockingStub;
+    private ManagedChannel channel;
+
+    @BeforeAll
+    public void setup(){
+        /** note that the channel talks to the proxy service**/
+        this.channel = ManagedChannelBuilder.forAddress("localhost",8585)
+                .usePlaintext()
+                .build();
+        this.bankServiceBlockingStub = BankServiceGrpc.newBlockingStub(channel);
+    }
+
+    @Test
+    public void loadBalancingDemo(){
+        for(int i=1;i<=10;i++){
+            var request  = BalanceCheckRequest.newBuilder()
+                    .setAccountNumber(i)
+                    .build();
+            var response = this.bankServiceBlockingStub.getAccountBalance(request);
+            log.info("{}", response);
+        }
+    }
+
+    @AfterAll
+    public void stop(){
+        this.channel.shutdownNow();
+    }
+
+}
