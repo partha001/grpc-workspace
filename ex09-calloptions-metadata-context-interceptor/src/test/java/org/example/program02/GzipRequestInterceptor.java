4baef0e0b1a5c87ae2b928f92fe01540709685bd
Partha Biswas <biswas.partha1@gmail.com>
2025-04-20 17:25:54 +0530

    ex09-program02 : interceptor: gzip using interceptor

---
+++ ex09-calloptions-metadata-context-interceptor/src/test/java/org/example/program02/GzipRequestInterceptor.java
@@ -0,0 +1,12 @@
+package org.example.program02;
+
+import io.grpc.*;
+
+public class GzipRequestInterceptor implements ClientInterceptor {
+
+
+    @Override
+    public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> methodDescriptor, CallOptions callOptions, Channel channel) {
+        return channel.newCall(methodDescriptor, callOptions.withCompression("gzip"));
+    }
+}
