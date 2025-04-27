00de57cf8b6ba81dce7b003e5909e3ebea528372
Partha Biswas <biswas.partha1@gmail.com>
2025-04-27 13:35:31 +0530

    ex09 : program06: userSessionToken

--- ex09-calloptions-metadata-context-interceptor/src/test/java/org/example/program06/UserSessionTokenInterceptorTest2.java
+++ ex09-calloptions-metadata-context-interceptor/src/test/java/org/example/program06/UserSessionTokenInterceptorTest2.java
@@ -1,10 +1,11 @@
 package org.example.program06;
 
-import com.partha.program06.BalanceCheckRequest;
+import com.partha.program06.Money;
+import com.partha.program06.WithdrawRequest;
 import io.grpc.CallCredentials;
 import io.grpc.ClientInterceptor;
 import io.grpc.Metadata;
-import io.grpc.stub.MetadataUtils;
+import org.example.common.ResponseObserver;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +15,11 @@ import java.util.List;
 import java.util.concurrent.Executor;
 
 /**
- * this is the test for unary call and userk-session-token
+ * this is the test for server-streaming-call
  */
-public class UserSessionTokenInterceptorTest extends AbstractInterceptorTest {
+public class UserSessionTokenInterceptorTest2 extends AbstractInterceptorTest {
 
-    private static final Logger log = LoggerFactory.getLogger(UserSessionTokenInterceptorTest.class);
+    private static final Logger log = LoggerFactory.getLogger(UserSessionTokenInterceptorTest2.class);
     private static final Metadata.Key<String> API_KEY = Metadata.Key.of("api-key", Metadata.ASCII_STRING_MARSHALLER);
 
 
@@ -36,27 +37,28 @@ public class UserSessionTokenInterceptorTest extends AbstractInterceptorTest {
     }
 
 
-
     /**
-     * as per our server and token configuration we should get the below result considering its a unary call.
-     * so lets runt the test case and verify the same from the console logs.
+     * as per our server and token configuration we should see the below result considering its a streaming call .
+     * lets run the test case and verify the same from the console logs.
      * call with user-token-1 should go fine
      * call with user-token-2 should go fine
-     * call with user-token-3 should go fine
-     * call with user-token-4 should go fine
+     * call with user-token-3 should give PERMISSION_DENIED since we are using a streaming call with user-token-3
+     * call with user-token-4 should give PERMISSION_DENIED since we are using a streaming call with user-token-4
      * call with user-token-5 should give UNAUTHENTICATED
      */
     @Test
-    public void unaryUserCredentialsDemo() {
-
+    public void streamingUserCredentialsDemo() {
         for(int i=1;i<=5;i++){
-            var request = BalanceCheckRequest.newBuilder().setAccountNumber(1).build();
-            var response = this.blockingStub
-                    .withCallCredentials(new UserSessionToken("user-token-"+i)) //user-specific or call-specific details are passed using this
-                    .getAccountBalance(request);
-            log.info("{}", response);
+            var observer = ResponseObserver.<Money>create();
+            var request = WithdrawRequest.newBuilder()
+                    .setAccountNumber(i)
+                    .setAmount(30)
+                    .build();
+            this.stub
+                    .withCallCredentials(new UserSessionToken("user-token-"+i))
+                    .withdraw(request, observer);
+            observer.awaite();
         }
-
     }
 
 
@@ -74,7 +76,6 @@ public class UserSessionTokenInterceptorTest extends AbstractInterceptorTest {
         public void applyRequestMetadata(RequestInfo requestInfo, Executor executor, MetadataApplier metadataApplier) {
             var metadata = new Metadata();
             metadata.put(Constants.USER_TOKEN_KEY, TOKEN_FORMAT.formatted(Constants.BEARER, jwt));
-
             // now applying the metadata
             metadataApplier.apply(metadata);
         }
